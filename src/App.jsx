59833cb52d65b8284fcabdbe7022587988d531ae
Lucas da Silva Oliveira <lucas-silva-98@hotmail.com>
2023-02-18 00:22:17 -0300

    add browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Matters from "./components/Matters"
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Matters />
       <Calendar />
